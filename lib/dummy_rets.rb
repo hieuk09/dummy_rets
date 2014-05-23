9851e8bfcb72c60ecb9a089814d05c37e12b8e8d
HieuN+MaiP <hieu.nguyen+mai.phan@eastagile.com>
2014-05-23 15:33:21 +0700

    [f] Mock rets client

--- lib/dummy_rets.rb
+++ lib/dummy_rets.rb
@@ -1,2 +1,14 @@
-module DummyRets
+module Rets
+  class Client
+    def initialize(options = {})
+    end
+
+    def find(quantity, opts = {})
+      {}
+    end
+
+    def all_objects(opts = {})
+      []
+    end
+  end
 end
